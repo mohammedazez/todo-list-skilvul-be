426db2c4f1f5a69f7ba211cbf8392286c986588d
mohammedazez <muhamadaziz047@gmail.com>
2023-08-28 19:45:38 +0700

    add res send router

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,9 @@ import express from "express";
 import todoRoute from "./todoRoute.js";
 
 const router = express.Router();
+router.get("/", (req, res) => {
+  res.send("Todo list Skilvul, its working!");
+});
 
 router.use("/api/v1", todoRoute);
 export default router;
